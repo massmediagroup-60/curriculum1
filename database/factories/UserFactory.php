6a2c69d66991801ee0e3d11c57943613154bc630
Yevgen <gumeniukyevgen@gmail.com>
2019-09-15 15:37:13 +0300

    added api_token to user model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => Hash::make('secret'), // password
+        'api_token' => Str::random(60),
         'remember_token' => Str::random(10),
     ];
 });
